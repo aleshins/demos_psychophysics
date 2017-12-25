0db8c71e8756518db6a31bbf20b124357b3330a7
zlon <aleshins@gmail.com>
2017-12-25 01:43:44 +0100

    eslint added

--- SFM/demoSFM_simple.js
+++ SFM/demoSFM_simple.js
@@ -1,28 +1,30 @@
 
+/* global THREE */
+
 'use strict';
 
 // Define default parameters
-let generalParameters = { // define all backgroun
+const generalParameters = { // define all backgroun
   backgroundColor: 0xFFFFFF,
   shadowsEnabled: true,
 };
 // once everything is loaded, we run our Three.js stuff.
 function init() {
   // create a scene, that will hold all our elements such as objects, cameras and lights.
-  let scene = new THREE.Scene();
+  const scene = new THREE.Scene();
   // create a camera, which defines where we're looking at.
-  let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
   // create a render and set the size
-  let renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer();
 
   renderer.setClearColor(new THREE.Color(generalParameters.backgroundColor));
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.shadowMap.enabled = generalParameters.shadowsEnabled;
   // create the ground plane
-  let planeGeometry = new THREE.PlaneGeometry(80, 80);
-  let planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
-  let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  const planeGeometry = new THREE.PlaneGeometry(80, 80);
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial);
   plane.receiveShadow = true;
   // rotate and position the plane
   plane.rotation.x = -0.5 * Math.PI;
@@ -32,44 +34,36 @@ function init() {
   // add the plane to the scene
   scene.add(plane);
   // create a sphere
-  let parentGeometry = new THREE.SphereGeometry(20, 10, 10);
-  //var parentMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
-  //var parent = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  let vertices = parentGeometry.vertices;
-  let parentMesh = new THREE.Mesh();
+  const parentGeometry = new THREE.SphereGeometry(20, 10, 10);
+  const vertices = parentGeometry.vertices;
+  const parentMesh = new THREE.Mesh();
   for (let i = 0, l = vertices.length; i < l; i++) {
-      let child_sphereGeometry = new THREE.SphereGeometry(2, 10, 10);
-      let child_sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
-      let child_sphere = new THREE.Mesh(child_sphereGeometry, child_sphereMaterial);
-      child_sphere.position.x=vertices[i].x;
-      child_sphere.position.y=vertices[i].y;
-      child_sphere.position.z=vertices[i].z;
-      child_sphere.castShadow = true;
-      parentMesh.add(child_sphere);
+    const childSphereGeometry = new THREE.SphereGeometry(2, 10, 10);
+    const childSphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+    const childSphere = new THREE.Mesh(childSphereGeometry, childSphereMaterial);
+    childSphere.position.x = vertices[i].x;
+    childSphere.position.y = vertices[i].y;
+    childSphere.position.z = vertices[i].z;
+    childSphere.castShadow = true;
+    parentMesh.add(childSphere);
   }
 
   // add the sphere to the scene
-//   scene.add(sphere);
+
   scene.add(parentMesh);
   // position and point the camera to the center of the scene
-  /*camera.position.x = -30;
-  camera.position.y = 40;
-  camera.position.z = 30;
-  */
   camera.position.x = 0;
   camera.position.y = 0;
   camera.position.z = 100;
-
   camera.lookAt(scene.position);
   // add spotlight for the shadows
-  var spotLight = new THREE.SpotLight(0xffffff);
+  let spotLight = new THREE.SpotLight(0xffffff);
   spotLight.position.set(-100, 100, 50);
   spotLight.castShadow = true;
   scene.add(spotLight);
   // add the output of the renderer to the html element
-  document.getElementById("WebGL-output").appendChild(renderer.domElement);
+  document.getElementById('WebGL-output').appendChild(renderer.domElement);
   // call the render function
-  var step = 0;
   renderScene();
   function renderScene() {
       //stats.update();
